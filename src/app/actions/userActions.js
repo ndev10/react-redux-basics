423151f196ed1350d08f32c17254709aef97460c
devang.ghugharawala <devang.ghugharawala@nitorinfotech.net>
2017-04-23 12:22:15 +0530

    async-redux

--- src/app/actions/userActions.js
+++ src/app/actions/userActions.js
@@ -1,7 +1,25 @@
 export function setName(name) {
-    return {
+    /*return {
         type: "SET_NAME",
         payload: name
+    };*/
+
+    /*return (dispatch) => {
+        setTimeout(() => {
+            dispatch({
+                type: "SET_NAME",
+                payload: name
+            });
+        },2000);
+    }*/
+
+     return {
+        type: "SET_NAME",
+        payload: new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(name);
+            }, 2000);
+        })
     };
 }
 
